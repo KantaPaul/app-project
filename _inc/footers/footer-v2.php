7427c969106e6aebb37bfd6dc0bdea5473806099
KantaPaul <pobon.paul1994@gmai.com>
2018-07-27 02:38:38 +0600

    here a maid done

---
+++ _inc/footers/footer-v2.php
@@ -0,0 +1,13 @@
+		<!-- footer section start -->
+		<footer class="<?php echo $klass; ?>">
+			<!-- <div class="footer-content">
+				<span class="border-separetor"></span>
+				<h2>CALL A MAID</h2>
+			</div> -->
+		</footer>
+		<!-- footer section end -->	
+		<!-- js file start -->
+		<?php get_section('footer-script', ''); ?>
+		<!-- End js file -->
+	</body>
+</html>
--- _inc/sections/here-a-maid.php
+++ _inc/sections/here-a-maid.php
@@ -1,7 +1,29 @@
-<section class="<?php echo $klass; ?>">
+<div class="<?php echo $klass; ?>">
     <div class="container">
-        <div class="row">
-            
-        </div><!-- .row END -->
+        <div class="here-a-maid-area">
+            <form action="#" class="here-a-maid-from" method="get">
+                <div class="inline-inputs clearfix">
+                    <div class="form-group">
+                        <select name="#" class="form-control" id="select-one">
+                            <option selected>Select</option>
+                            <option value="one">One</option>
+                            <option value="teo">Two</option>
+                            <option value="three">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="#" class="form-control" id="select-two">
+                            <option selected>Select</option>
+                            <option value="one">One</option>
+                            <option value="teo">Two</option>
+                            <option value="three">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <input type="submit" class="btn btn-primary" value="CONTINUE">
+                </div>
+            </form>
+        </div>
     </div><!-- .container END -->
-</section>
+</div>
